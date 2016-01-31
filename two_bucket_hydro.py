f6921d0576598752342264a2babe373238f5a94c
Greg Tucker <gtucker@colorado.edu>
2016-01-31 16:04:21 -0700

    add draft ofcomponent code, copied from flexure.py

---
+++ two_bucket_hydro.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+"""Calculate soil moisture and runoff on a gridded landscape.
+
+Examples
+--------
+
+Create a grid on which we will run the calculations.
+
+>>> from landlab import RasterModelGrid
+>>> from landlab.components.flexure import TwoBucketHydro
+>>> grid = RasterModelGrid((5, 4), spacing=(1.e4, 1.e4))
+
+Check the fields that are used as input to the two_bucket_hydro component.
+
+>>> TwoBucketHydro.input_var_names # doctest: +NORMALIZE_WHITESPACE
+('lithosphere__overlying_pressure_increment',)
+
+Check the units for the fields.
+
+#>>> TwoBucketHydro.var_units('top_layer__XXX')
+#'Pa'
+
+If you are not sure about one of the input or output variables, you can
+get help for specific variables.
+
+#>>> TwoBucketHydro.var_help('lithosphere__overlying_pressure_increment')
+#name: lithosphere__overlying_pressure_increment
+#description:
+#  Applied pressure to the lithosphere over a time step
+#units: Pa
+#at: node
+#intent: in
+#
+#>>> flex = Flexure(grid)
+#
+#In creating the component, a field (initialized with zeros) was added to the
+#grid. Reset the interior nodes for the loading.
+#
+#>>> dh = grid.at_node['lithosphere__overlying_pressure_increment']
+#>>> dh = dh.reshape(grid.shape)
+#>>> dh[1:-1, 1:-1] = flex.gamma_mantle
+#
+#>>> flex.update()
+#
+#>>> flex.output_var_names
+#('lithosphere__elevation_increment',)
+#>>> flex.grid.at_node['lithosphere__elevation_increment']
+#...     # doctest: +NORMALIZE_WHITESPACE
+#array([ 0., 0., 0., 0.,
+#        0., 1., 1., 0.,
+#        0., 1., 1., 0.,
+#        0., 1., 1., 0.,
+#        0., 0., 0., 0.])
+"""
+
+import numpy as np
+
+from landlab import Component
+
+
+class TwoBucketHydro(Component):
+
+    """Calculate soil moisture and runoff on a gridded landscape.
+
+    Landlab component that implements a two-bucket distributed hydrology model.
+    """
+#    Construction::
+#
+#        Flexure(grid, eet=65e3, youngs=7e10, method='airy', rho_mantle=3300.,
+#                gravity=9.80665)
+#
+#    Parameters
+#    ----------
+#    grid : RasterModelGrid
+#        A grid.
+#    eet : float, optional
+#        Effective elastic thickness (m).
+#    youngs : float, optional
+#        Young's modulus.
+#    method : {'airy', 'flexure'}, optional
+#        Method to use to calculate deflections.
+#    rho_mantle : float, optional
+#        Density of the mantle (kg / m^3).
+#    gravity : float, optional
+#        Acceleration due to gravity (m / s^2).
+#
+#    Examples
+#    --------
+#    >>> from landlab import RasterModelGrid
+#    >>> from landlab.components.flexure import Flexure
+#    >>> grid = RasterModelGrid((5, 4), spacing=(1.e4, 1.e4))
+#
+#    >>> flex = Flexure(grid)
+#    >>> flex.name
+#    'Flexure'
+#    >>> flex.input_var_names
+#    ('lithosphere__overlying_pressure_increment',)
+#    >>> flex.output_var_names
+#    ('lithosphere__elevation_increment',)
+#    >>> sorted(flex.units) # doctest: +NORMALIZE_WHITESPACE
+#    [('lithosphere__elevation_increment', 'm'),
+#     ('lithosphere__overlying_pressure_increment', 'Pa')]
+#
+#    >>> flex.grid.number_of_node_rows
+#    5
+#    >>> flex.grid.number_of_node_columns
+#    4
+#    >>> flex.grid is grid
+#    True
+#
+#    >>> np.all(grid.at_node['lithosphere__elevation_increment'] == 0.)
+#    True
+#
+#    >>> np.all(grid.at_node['lithosphere__overlying_pressure_increment'] == 0.)
+#    True
+#    >>> flex.update()
+#    >>> np.all(grid.at_node['lithosphere__elevation_increment'] == 0.)
+#    True
+#
+#    >>> load = grid.at_node['lithosphere__overlying_pressure_increment']
+#    >>> load[4] = 1e9
+#    >>> dz = grid.at_node['lithosphere__elevation_increment']
+#    >>> np.all(dz == 0.)
+#    True
+#
+#    >>> flex.update()
+#    >>> np.all(grid.at_node['lithosphere__elevation_increment'] == 0.)
+#    False
+#    """
+
+    _name = 'TwoBucketHydro'
+
+    _input_var_names = ()
+
+    _output_var_names = (
+        'upper_layer__water_content',
+        'lower_layer__water_content'
+    )
+
+    _var_units = {
+        'upper_layer__water_content': 'm',
+        'lower_layer__water_content': 'm',
+    }
+
+    _var_mapping = {
+        'upper_layer__water_content': 'node',
+        'lower_layer__water_content': 'node',
+    }
+
+    _var_doc = {
+        'upper_layer__water_content':
+            'Water volume per unit area in upper layer',
+        'lower_layer__water_content':
+            'Water volume per unit area in lower layer',
+    }
+
+    def __init__(self, grid, rain_rate=0.001, **kwds):
+        """Initialize the flexure component.
+
+        Parameters
+        ----------
+        grid : RasterModelGrid
+            A grid.
+        rain_rate : float, optional
+            Rainfall or snowmelt rate (m/hr).
+        """
+
+        self._rain_rate = rain_rate
+
+        self._grid = grid
+
+        super(TwoBucketHydro, self).__init__(grid, **kwds)
+
+        for name in self._input_var_names:
+            if name not in self.grid.at_node:
+                self.grid.add_zeros('node', name, units=self._var_units[name])
+
+        for name in self._output_var_names:
+            if name not in self.grid.at_node:
+                self.grid.add_zeros('node', name, units=self._var_units[name])
+
+        self._r = self._set_kei_func_grid()
+
+    @property
+    def eet(self):
+        """Effective elastic thickness (m)."""
+        return self._eet
+
+    @eet.setter
+    def eet(self, new_val):
+        if new_val <= 0:
+            raise ValueError('Effective elastic thickness must be positive.')
+        self._r = self._set_kei_func_grid()
+        self._eet = new_val
+
+    @property
+    def youngs(self):
+        """Young's modulus of lithosphere (Pa)."""
+        return self._youngs
+
+    @property
+    def rho_mantle(self):
+        """Density of mantle (kg/m^3)."""
+        return self._rho_mantle
+
+    @property
+    def gamma_mantle(self):
+        """Specific density of mantle (N/m^3)."""
+        return self._rho_mantle * self._gravity
+
+    @property
+    def gravity(self):
+        """Acceleration due to gravity (m/s^2)."""
+        return self._gravity
+
+    @property
+    def method(self):
+        """Name of method used to calculate deflections."""
+        return self._method
+
+    @property
+    def alpha(self):
+        """Flexure parameter (m)."""
+        return get_flexure_parameter(self._eet, self._youngs, 2,
+                                     gamma_mantle=self.gamma_mantle)
+
+    def _set_kei_func_grid(self):
+        from scipy.special import kei
+
+        dx, dy = np.meshgrid(
+            np.arange(self._grid.number_of_node_columns) * self._grid.dx,
+            np.arange(self._grid.number_of_node_rows) * self._grid.dy)
+
+        return kei(np.sqrt(dx ** 2 + dy ** 2) / self.alpha)
+
+    def update(self, n_procs=1):
+        """Update fields with current loading conditions.
+
+        Parameters
+        ----------
+        n_procs : int, optional
+            Number of processors to use for calculations.
+        """
+        load = self.grid.at_node['lithosphere__overlying_pressure_increment']
+        deflection = self.grid.at_node['lithosphere__elevation_increment']
+
+        new_load = load.copy()
+
+        deflection.fill(0.)
+
+        if self._method == 'airy':
+            deflection[:] = new_load / self.gamma_mantle
+        else:
+            self.subside_loads(new_load, deflection=deflection,
+                               n_procs=n_procs)
+
+    def subside_loads(self, loads, deflection=None, n_procs=1):
+        """Subside surface due to multiple loads.
+
+        Parameters
+        ----------
+        loads : ndarray of float
+            Loads applied to each grid node.
+        deflection : ndarray of float, optional
+            Buffer to place resulting deflection values.
+        n_procs : int, optional
+            Number of processors to use for calculations.
+
+        Returns
+        -------
+        ndarray of float
+            Deflections caused by the loading.
+        """
+        if deflection is None:
+            deflection = np.empty(self.shape, dtype=np.float)
+
+        from .cfuncs import subside_grid_in_parallel
+
+        w = deflection.reshape(self._grid.shape)
+        load = loads.reshape(self._grid.shape)
+
+        subside_grid_in_parallel(w, load * self._grid.dx * self._grid.dy,
+                                 self._r, self.alpha, self.gamma_mantle,
+                                 n_procs)
+
+        return deflection
